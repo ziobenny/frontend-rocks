af9b1eafb80cb84e2168e05a66db6c82a93c1cdd
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2025-03-01 14:53:16 +0100

    style boilerplate page

--- frontend-rocks/src/App.tsx
+++ frontend-rocks/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-dvh flex flex-col items-center justify-center">
+      <div className="bg-white p-8 rounded-md shadow-lg">
+        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
+          Frontend Rocks
+        </h1>
 
-export default App
+        <div className="flex justify-center space-x-4 mb-2">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} alt="React logo" />
+          </a>
+        </div>
+
+        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
+
+        <div className="flex flex-col items-center space-y-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
+          </button>
+          <p className="text-center">
+            Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
+          </p>
+        </div>
+
+        <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
+      </div>
+    </div>
+  );
+}
