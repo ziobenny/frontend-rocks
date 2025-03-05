f25e056c373e239cedee5cf3acc7099a677417b9
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2025-03-05 18:19:28 +0100

    move root folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? 'https://giovannigiordano.github.io/frontend-rocks/' : '/',
   plugins: [react(), tailwindcss()],
 })
